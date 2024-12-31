2520a0f365f0942e446f88e0b570687e38f5fd85
Arthur BERTIN <133151986+SlayArt@users.noreply.github.com>
2024-12-31 11:32:13 +0100

    code_1.py
    result more lisible, some bug stay...

--- code_1.py
+++ code_1.py
@@ -1,5 +1,8 @@
 from PIL import Image
-import numpy as np
+
+def drawing_in_file(charactere):
+    with open("ascii_color_drawing.txt", "a+") as file:
+        file.write(charactere)
 
 def ask_user():
     image_user = input("what is the link to the image you wanna transform ? ")
@@ -7,31 +10,33 @@ def ask_user():
     return img.convert('RGB')
 
 def size_image(image):
-    return image.size
+    width, height = image.size
+    aspect_ratio = width / height
+    new_width = 150
+    new_height = int(new_width / aspect_ratio)
 
-def traitement(image, width, height, tableau):
-    for x in range(width):
-        for y in range(height):
+    resized_img = image.resize((new_width, new_height), Image.LANCZOS)
+
+    return resized_img
+
+def traitement(image, width, height):
+    for y in range(height):
+        for x in range(width):
             r, g, b = image.getpixel((x, y))
             avg = (r + g + b)/3
 
             if avg > 0 and avg < 55:
-                tableau[x][y] = "$"
+                drawing_in_file("$")
             elif avg > 200 and avg < 255:
-                tableau[x][y] = "|"
-    
-    return tableau
-
-def np_tab(image):
-    height, width = size_image(image)
-    tableau = np.full((height, width), "", dtype = str)
-    return tableau
+                drawing_in_file("|")
+        drawing_in_file(" END OF A LINE ... end of a line ...\n") 
 
 def main():
+    print("STARTING... ")
     image = ask_user()
-    tab = np_tab(image)
-    size = size_image(image)
-    new_tab = traitement(image, size[0], size[1], tab)
-    print(new_tab)
+    print("IN WORKING... ")
+    resized_img = size_image(image)
+    traitement(resized_img, resized_img.width, resized_img.height)
+    print("WORK END... PROCESS OCCURE WITH NO ISSUE... ")
 
 main()
